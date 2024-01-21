7253009dc2fb4f254196a590699442b555952841
dmMaksymenko <dmitriy.maksimenko928@gmail.com>
2024-01-21 15:31:27 +0000

    finish both homeworks, added styles

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,46 @@
+'use strict';
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
 
+function saveToLS(key = 'empty', value = '') {
+  const jsonData = JSON.stringify(value);
+  localStorage.setItem(key, jsonData);
+}
+function loadFromLS(key) {
+  const data = localStorage.getItem(key);
+
+  try {
+    const result = JSON.parse(data);
+    return result;
+  } catch {
+    return data;
+  }
+}
+
+form.addEventListener('input', e => {
+  const userEmail = form.elements.email.value;
+  const userMessage = form.elements.message.value;
+
+  const data = {
+    email: userEmail,
+    message: userMessage,
+  };
+  saveToLS(STORAGE_KEY, data);
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const data = loadFromLS(STORAGE_KEY) || {};
+  console.log(data);
+
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+});
+
+function loadDataToForm() {
+  const data = loadFromLS(STORAGE_KEY) || {};
+  form.elements.email.value = data.email || '';
+  form.elements.message.value = data.message || '';
+}
+loadDataToForm();
